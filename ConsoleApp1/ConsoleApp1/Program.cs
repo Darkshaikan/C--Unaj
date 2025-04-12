4ffcaaa19c589a19eaa25981dc89d1797cd8b06a
German Di Paola <113396491+Darkshaikan@users.noreply.github.com>
2025-04-12 10:22:40 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Desarrollar un programa donde  se le solicite al usuario ingresar apellido y calificacion dado esos parametros validar si el alumno esta aprobado o no y le indique con un mensaje el resultado.*/
+
+String nombre, apellido;
+int nota;
+
+System.Console.WriteLine("Ingrese su nombre apellido y nota");
+nombre = Console.ReadLine();
+apellido = Console.ReadLine(); 
+nota = Convert.ToInt32(Console.ReadLine());
+
+if (nota >= 7)
+{
+    System.Console.WriteLine(nombre + " " + apellido + " " + "Esta Aprobado");
+
+}
+else
+{
+    System.Console.WriteLine(nombre + " " + apellido + " " + "No esta Aprobado");
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Ingrese Nombre, Apellido y Nota");
+            String nombre, apellido;
+            int nota;
+            nombre = Console.ReadLine();
+            apellido = Console.ReadLine();
+            nota = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (nota >= 0)
+            {
+                System.Console.WriteLine(nombre + " " + apellido + " " + "Esta aprobado");
+            }
+            else
+            {
+                System.Console.WriteLine(nombre + "" + apellido + "" + "No esta Aprobado");
+            }
+        }
+    }
+}
